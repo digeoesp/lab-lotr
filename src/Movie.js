3c257e32e08dbdd9512f060d783d0263e2cbac59
digeoesp <diegocrypto@outlook.com>
2021-05-04 22:05:55 -0700

    added component

---
+++ src/Movie.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Movie = () => {
+    return (
+        <div>
+           <h1>The Lord of the Rings: A Trilogy</h1>
+           <p>4h 37min</p>
+        </div>
+    )
+}
+
+export default Movie
